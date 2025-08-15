9a47d2fe835e6a3b5e95fb4680255041193566a4
wWellingtonLima <0wellingtonlima2@gmail.com>
2025-08-15 17:55:38 +0100

    add ssl require to database.js

--- infra/database.js
+++ infra/database.js
@@ -7,6 +7,7 @@ async function query(queryObj) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: true
   });
   await client.connect();
 
