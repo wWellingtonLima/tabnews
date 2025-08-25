3981521df6ec4c0ef0706bb68aac94483f4838bc
wWellingtonLima <0wellingtonlima2@gmail.com>
2025-08-25 14:30:39 +0100

    add packages `concurrently` and `async-retry` to make npm-test script

---
+++ tests/orchestrator.js
@@ -0,0 +1,25 @@
+import retry from "async-retry";
+
+async function waitForAllServices() {
+  await waitForWebServer();
+
+  async function waitForWebServer() {
+    await retry(fetchStatusPage, {
+      retries: 100,
+      maxTimeout: 500,
+      minTimeout: 100,
+    });
+
+    async function fetchStatusPage() {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+
+      if (response.status !== 200) {
+        throw Error();
+      }
+    }
+  }
+}
+
+export default {
+  waitForAllServices,
+};
