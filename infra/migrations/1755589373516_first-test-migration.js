38ace700933122d0ecfc2b717581deb23f90742f
wWellingtonLima <0wellingtonlima2@gmail.com>
2025-08-26 19:48:32 +0100

    ci: add commitlint with conventional commits config

--- infra/migrations/1755589373516_first-test-migration.js
+++ infra/migrations/1755589373516_first-test-migration.js
@@ -1,7 +1,7 @@
-/* eslint-disable camelcase */
-
 exports.shorthands = undefined;
 
+// eslint-disable-next-line no-unused-vars
 exports.up = (pgm) => {};
 
+// eslint-disable-next-line no-unused-vars
 exports.down = (pgm) => {};
